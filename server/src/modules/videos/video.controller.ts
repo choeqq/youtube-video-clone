476849655d09c59f9661856dbbc2478819215146
choeqq <k.chola@op.pl>
2022-09-18 16:33:51 +0200

    find videos

--- server/src/modules/videos/video.controller.ts
+++ server/src/modules/videos/video.controller.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { Video } from "./video.model";
-import { createVideo, findVideo } from "./video.service";
+import { createVideo, findVideo, findVideos } from "./video.service";
 import { UpdateVideoBody, UpdateVideoParams } from "./video.schema";
 
 const MIME_TYPES = ["video/mp4"];
@@ -85,4 +85,10 @@ export async function updateVideoHandler(
   await video.save();
 
   return res.status(StatusCodes.OK).send(video);
+}
+
+export async function findVideosHandler(_: Request, res: Response) {
+  const videos = await findVideos();
+
+  return res.status(StatusCodes.OK).send(videos);
 }
--- server/src/modules/videos/video.route.ts
+++ server/src/modules/videos/video.route.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import requireUser from "../../middleware/requireUser";
-import { updateVideoHandler, uploadVideoHandler } from "./video.controller";
+import {
+  findVideosHandler,
+  updateVideoHandler,
+  uploadVideoHandler,
+} from "./video.controller";
 
 const router = express.Router();
 
 router.post("/", requireUser, uploadVideoHandler);
 
 router.patch("/:videoId", requireUser, updateVideoHandler);
+
+router.get("/", findVideosHandler);
 
 export default router;
