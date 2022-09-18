8f7cfbbbce63ffb09202c2996bfc2744561dc537
choeqq <k.chola@op.pl>
2022-09-18 16:45:39 +0200

    stream video

--- server/src/modules/videos/video.controller.ts
+++ server/src/modules/videos/video.controller.ts
@@ -8,6 +8,8 @@ import { UpdateVideoBody, UpdateVideoParams } from "./video.schema";
 
 const MIME_TYPES = ["video/mp4"];
 
+const CHUNK_SIZE_IN_BYTES = 1000000; // 1 mb
+
 function getPath({
   videoId,
   extension,
@@ -85,6 +87,55 @@ export async function updateVideoHandler(
   await video.save();
 
   return res.status(StatusCodes.OK).send(video);
+}
+
+export async function streamVideoHandler(req: Request, res: Response) {
+  const { videoId } = req.params;
+
+  const range = req.headers.range;
+
+  if (!range) {
+    return res.status(StatusCodes.BAD_REQUEST).send("Range must be provided");
+  }
+
+  const video = await findVideo(videoId);
+
+  if (!video) {
+    return res.status(StatusCodes.NOT_FOUND).send("Video not found");
+  }
+
+  const filePath = getPath({
+    videoId: video.videoId,
+    extension: video.extension,
+  });
+
+  const fileSizeInBytes = fs.statSync(filePath).size;
+
+  const chunkStart = Number(range.replace(/\D/g, ""));
+
+  const chunkEnd = Math.min(
+    chunkStart + CHUNK_SIZE_IN_BYTES,
+    fileSizeInBytes - 1
+  );
+
+  const contentLength = chunkEnd - chunkStart + 1;
+
+  const headers = {
+    "Content-Range": `bytes ${chunkStart}-${chunkEnd}/${fileSizeInBytes}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": `video/${video.extension}`,
+    // "Cross-Origin-Resource-Policy": "cross-origin",
+  };
+
+  res.writeHead(StatusCodes.PARTIAL_CONTENT, headers);
+
+  const videoStream = fs.createReadStream(filePath, {
+    start: chunkStart,
+    end: chunkEnd,
+  });
+
+  videoStream.pipe(res);
 }
 
 export async function findVideosHandler(_: Request, res: Response) {
--- server/src/modules/videos/video.route.ts
+++ server/src/modules/videos/video.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import requireUser from "../../middleware/requireUser";
 import {
   findVideosHandler,
+  streamVideoHandler,
   updateVideoHandler,
   uploadVideoHandler,
 } from "./video.controller";
@@ -11,6 +12,8 @@ const router = express.Router();
 router.post("/", requireUser, uploadVideoHandler);
 
 router.patch("/:videoId", requireUser, updateVideoHandler);
+
+router.get("/:videoId", streamVideoHandler);
 
 router.get("/", findVideosHandler);
 
