65833bb4938a05c66f26a427b03e5a7c0ab4345c
choeqq <k.chola@op.pl>
2022-09-18 20:41:38 +0200

    login page

--- client/pages/auth/login.tsx
+++ client/pages/auth/login.tsx
@@ -1,5 +1,71 @@
+import {
+  Button,
+  Container,
+  Paper,
+  PasswordInput,
+  Stack,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { showNotification, updateNotification } from "@mantine/notifications";
+import { AxiosError } from "axios";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useMutation } from "react-query";
+import { login } from "../api";
+
 function LoginPage() {
-  return <div>Login Page</div>;
+  const router = useRouter();
+
+  const form = useForm({
+    initialValues: {
+      email: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const mutation = useMutation<
+    string,
+    AxiosError,
+    Parameters<typeof login>["0"]
+  >(login, {
+    onSuccess: () => {
+      router.push("/");
+    },
+  });
+
+  return (
+    <div>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <Container>
+        <Title>Login</Title>
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <form onSubmit={form.onSubmit((values) => mutation.mutate(values))}>
+            <Stack>
+              <TextInput
+                label="Email"
+                placeholder="kacper@example.com"
+                required
+                {...form.getInputProps("email")}
+              />
+              <PasswordInput
+                label="Password"
+                placeholder="Your password"
+                required
+                {...form.getInputProps("password")}
+              />
+              <Button type="submit">Login</Button>
+            </Stack>
+          </form>
+        </Paper>
+      </Container>
+    </div>
+  );
 }
 
 export default LoginPage;
